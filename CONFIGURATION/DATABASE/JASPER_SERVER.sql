292f224350b275de2bb4d94362fb7ebf490d732a
Andrey Baluevsky <a-baluevsky@users.noreply.github.com>
2015-06-26 14:20:29 +0300

    JASPER SERVER param

--- CONFIGURATION/DATABASE/JASPER_SERVER.sql
+++ CONFIGURATION/DATABASE/JASPER_SERVER.sql
@@ -7,7 +7,8 @@ INSERT INTO CUD_TEST.SETTINGS_KNL_T (ID_SRV,
                                      CREATED,
                                      DATE_EVENT_SRV,
                                      ATTEMPT_SRV,
-                                     DESCRIPTION)
+                                     DESCRIPTION,
+									 NAME_PARAM)
      VALUES (9,
              'JASPER_SERVER',
              'http://192.168.68.37:8080/jasperserver/rest_v2/reports/',
@@ -15,6 +16,7 @@ INSERT INTO CUD_TEST.SETTINGS_KNL_T (ID_SRV,
              TO_DATE ('06/19/2015 00:00:00', 'MM/DD/YYYY HH24:MI:SS'),
              TO_DATE ('06/19/2015 00:00:00', 'MM/DD/YYYY HH24:MI:SS'),
              1,
-             'Ѕазовый путь к отчЄтам JASPER SERVER ');
+             'Ѕазовый путь к отчЄтам JASPER SERVER ',
+			 'JASPER_SERVER');
 
 COMMIT;
